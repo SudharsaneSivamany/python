4fcaea5837067492c33596021541144e7ba3cf3a
SudharsaneSivamany <sudharsanes694@gmail.com>
2022-10-11 04:39:40 +0000

    added career and redirect function in flask app

--- flask/hello_world.py
+++ flask/hello_world.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return "Hello World"
+
+
+@app.route('/career')
+def career():
+    return "Have a good career"
+
+
+@app.route('/red')
+def red():
+    return redirect(url_for("hello_world"))
 
 if __name__ == '__main__':
     app.run()
