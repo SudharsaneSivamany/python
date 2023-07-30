28eb9e6384ed4885cd715cbba8c5ebe84b4af28d
Sudharsane Sivamany <89003990+SudharsaneSivamany@users.noreply.github.com>
2023-07-30 17:29:09 +0530

    Create rain_trap.py

---
+++ class/rain_trap.py
@@ -0,0 +1,16 @@
+class solv:
+    def trap(self, a):
+        water_occupied = 0
+        for height in range(1,max(a)+1):
+            stone = []
+            full_length = []
+            for ind in range(len(a)):
+                if a[ind] >= height:
+                    stone.append(ind)
+            for length in range(min(stone),max(stone)+1):
+                full_length.append(length)
+                water_occupied += len(set(full_length)-set(stone))
+        return water_occupied
+a = [0,1,2,2,3,0,4]
+sol = solv()
+print(sol.trap(a))
